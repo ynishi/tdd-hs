1fe62ac43c9cfbf4e65886f050d35b8850074c2c
Yutaka Nishimura <ytk.nishimura@gmail.com>
2019-09-08 15:54:55 +0900

    19.1 add testSetUp

--- src/Xunit.hs
+++ src/Xunit.hs
@@ -23,21 +23,32 @@ data WasRun = WasRun
 testMethod :: WasRun -> (WasRun -> IO WasRun)
 testMethod _ = \x -> return $ WasRun (testCase x) True
 
-test = WasRun (TestCase "testMethod") False
-
 data TestCaseTest =
   TestCaseTest Name
 
 instance TC TestCaseTest where
-  method = testRunning
+  method t@(TestCaseTest x) =
+    case x of
+      "testRunning" -> testRunning t
+      "testSetUp"   -> testSetUp t
 
 testRunning _ =
   \x -> do
+    let test = WasRun (TestCase "testMethod") False
     assert (not . wasRun $ test) dummy
     tested <- run test
     assert (wasRun tested) dummy
     return x
 
+testSetUp _ =
+  \x -> do
+    let test = WasRun (TestCase "testMethod") False
+    tested <- run test
+    assert (wasSetUp tested) dummy
+    return x
+
 dummy = putStr ""
 
-main = run $ TestCaseTest "testRunning"
+main = do
+  run $ TestCaseTest "testRunning"
+  run $ TestCaseTest "testSetUp"
