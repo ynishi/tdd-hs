7209786611251c5ef9737d5acac08349a1fb4674
Yutaka Nishimura <ytk.nishimura@gmail.com>
2019-09-08 12:14:00 +0900

    18.8 run just call testMethod

--- src/Xunit.hs
+++ src/Xunit.hs
@@ -10,6 +10,9 @@ wasRun (WasRun _ x) = x
 testMethod :: WasRun -> IO WasRun
 testMethod (WasRun s _) = return $ WasRun s True
 
+run :: WasRun -> IO WasRun
+run = testMethod
+
 test = WasRun "testMethod" False
 
 main = do
